e2818b9754a8c30ad8f0bc17987f8b8c00c9a79f
Darren Koomson <darrenkoomson17@gmail.com>
2020-04-21 22:47:53 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,36 @@
 def addition(num1, num2)
+  sum = num1 + num2
+  return sum
 
 end
 
 def subtraction(num1, num2)
+  difference = num1 - num2
+  return difference
+
 
 end
 
 def division(num1, num2)
+  quotient = num1/num2
+  return quotient
 
 end
 
 def multiplication(num1, num2)
+  product = num1*num2
+  return product
 
 end
 
 def modulo(num1, num2)
+  ans = num1%num2
+  return ans
 
 end
 
 def square_root(num)
+  sqrt = Math.sqrt(num)
+  return sqrt
 
 end
