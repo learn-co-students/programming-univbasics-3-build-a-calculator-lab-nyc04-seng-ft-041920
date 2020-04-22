cd2fbaca516205c33432d010f3dfc786f797d132
Leonora Squires <leonora.squires@gmail.com>
2020-04-22 19:30:41 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,28 @@
 def addition(num1, num2)
-
+sum = num1 + num2
+sum
 end
 
 def subtraction(num1, num2)
-
+dif = num1 - num2
+dif
 end
 
 def division(num1, num2)
-
+div = num1 / num2
+div
 end
 
 def multiplication(num1, num2)
-
+mul = num1 * num2
+mul
 end
 
 def modulo(num1, num2)
-
+mod = num1 % num2
+mod
 end
 
 def square_root(num)
-
+Math.sqrt(num)
 end
